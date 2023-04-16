2113b51810fe60e30536f2d95ff781a5489b2ebd
Pratik Khaire <pratikkhaire2004.com>
2023-04-16 12:34:44 +0530

    This is the final stage of project

--- app/src/main/java/com/example/myapplication/QuizPage.java
+++ app/src/main/java/com/example/myapplication/QuizPage.java
@@ -29,22 +29,121 @@ public class QuizPage extends AppCompatActivity {
     Intent intent;
 
     private String[] questions = {
-            "What is the capital of France?",
-            "What is the largest continent in the world?",
-            "What is the smallest country in the world by land area?",
-            "What is the highest mountain in the world?",
-            "What is the largest planet in our solar system?"
+//            "Have you experienced any significant changes in your mood or behavior lately?",
+//            "Have you noticed any changes in your sleep patterns, such as difficulty falling asleep or staying asleep?",
+//            "Have you experienced a loss of interest in activities that you used to enjoy?",
+//            "Have you had thoughts of self-harm or suicide?",
+//            "Have you been feeling more irritable or agitated than usual?"
+            "I found myself getting upset by quite trivial things",
+            "I was aware of dryness of my mouth",
+            "I couldn't seem to experience any positive feeling at all",
+            "I experienced breathing difficulty (eg, excessively rapid breathing,\n" +
+                    "breathlessness in the absence of physical exertion)",
+            "I just couldn't seem to get going",
+            "I tended to over-react to situations",
+            "I had a feeling of shakiness (eg, legs going to give way)",
+            "I found it difficult to relax",
+            "I found myself in situations that made me so anxious I was most\n" +
+                    "relieved when they ended\n",
+            "I felt that I had nothing to look forward to",
+            "I found myself getting upset rather easily",
+            "I felt that I was using a lot of nervous energy",
+            "I felt sad and depressed",
+            "I found myself getting impatient when I was delayed in any way\n" +
+                    "(eg, elevators, traffic lights, being kept waiting)\n",
+            "I had a feeling of faintness",
+            "I felt that I had lost interest in just about everything ",
+            "I felt I wasn't worth much as a person ",
+            "I felt that I was rather touchy",
+            "I perspired noticeably (eg, hands sweaty) in the absence of high\n" +
+                    "temperatures or physical exertion",
+            "I felt scared without any good reason",
+            "I felt that life wasn't worthwhile",
+            "I found it hard to wind down ",
+            "I had difficulty in swallowing ",
+            "I couldn't seem to get any enjoyment out of the things I did",
+            "I was aware of the action of my heart in the absence of physical\n" +
+                    "exertion (eg, sense of heart rate increase, heart missing a beat)\n",
+            "I felt down-hearted and blue",
+            "I found that I was very irritable",
+            "I felt I was close to panic",
+            "I found it hard to calm down after something upset me",
+            "I feared that I would be \"thrown\" by some trivial but\n" +
+                    "unfamiliar task",
+            "I was unable to become enthusiastic about anything",
+            "I found it difficult to tolerate interruptions to what I was doing",
+            " I was in a state of nervous tension",
+            "I felt I was pretty worthless",
+            "I was intolerant of anything that kept me from getting on with\n" +
+                    "what I was doing",
+            "I felt terrified",
+            "I could see nothing in the future to be hopeful about",
+            "I felt that life was meaningless",
+            "I found myself getting agitated",
+            "I was worried about situations in which I might panic and make\n" +
+                    "a fool of myself",
+            "I experienced trembling (eg, in the hands)",
+            "I found it difficult to work up the initiative to do things"
     };
 
     private String[][] answers = {
-            {"New York", "London", "Paris", "Tokyo"},
-            {"Asia", "Africa", "North America", "Europe"},
-            {"Monaco", "Nauru", "Vatican City", "San Marino"},
-            {"Mount Kilimanjaro", "Mount Everest", "Denali", "Matterhorn"},
-            {"Jupiter", "Saturn", "Uranus", "Neptune"}
+//            {"0", "1", "2", "3"},
+//            {"Yes", "No", "Sometime", "Always"},
+//            {"Yes", "No", "Sometime", "Always"},
+//            {"Yes", "No", "Sometime", "Always"},
+//            {"Yes", "No", "Sometime", "Always"},
+//            {"Asia", "Africa", "North America", "Europe"},
+//            {"Monaco", "Nauru", "Vatican City", "San Marino"},
+//            {"Mount Kilimanjaro", "Mount Everest", "Denali", "Matterhorn"},
+//            {"Jupiter", "Saturn", "Uranus", "Neptune"}
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+            {"0", "1", "2", "3"},
+
     };
 
-    private int[] correctAnswers = {2, 1, 3, 2, 0};
+//    private int[] correctAnswers = {2, 1, 3, 2, 0};
+    private int[] correctAnswers = {1,1,1,1,1,1,1,1,1,1,11,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
 
     @SuppressLint("MissingInflatedId")
     @Override
